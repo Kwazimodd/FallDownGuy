80863c3906953793e6cab8f8ef25a02df910f2ab
DanyloDiachenko <mrcross622@gmail.com>
2021-10-26 18:31:12 +0300

    AngryBall

--- Assets/Scripts/Creatures/AngryBall.cs
+++ Assets/Scripts/Creatures/AngryBall.cs
@@ -1,19 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class AngryBall : MonoBehaviour
 {
     [SerializeField] private int health = 3;
     private Animator animator;
+    private Rigidbody2D rigidbody2D;
+    private float attackTime;
+    private bool isAttacking;
+    [SerializeField] private int speed;
+    [SerializeField] private Transform target;
+    [SerializeField] private float attackRange;
+    [SerializeField] private bool canDoDamage;
+    
+    public bool CanDoDamage
+    {
+        get => canDoDamage;
+        set => canDoDamage = value;
+    }
+    public float AttackRange
+    {
+        get => attackRange;
+        set => attackRange = value;
+    }
+    public Transform Target
+    {
+        get => target;
+        set => target = value;
+    }
+    public bool IsAttacking
+    {
+        get => isAttacking;
+        set => isAttacking = value;
+    }
 
+    public float AttackTime
+    {
+        get => attackTime;
+        set => attackTime = value;
+    }
     public Animator Animator => this.animator;
 
     private IState currentState;
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Start()
     {
-        
+        ChangeState(new StaticState());
     }
 
     // Update is called once per frame
@@ -27,7 +68,7 @@ public class AngryBall : MonoBehaviour
         health -= damage;
     }
 
-    public void changeState(IState newState)
+    public void ChangeState(IState newState)
     {
         if (currentState != null)
         {
@@ -36,5 +77,10 @@ public class AngryBall : MonoBehaviour
 
         currentState = newState;
         currentState.Enter(this);
+    }
+
+    public void MoveTo(Vector2 direction)
+    {
+        rigidbody2D.velocity = direction * speed;
     }
 }
---
+++ Assets/Scripts/Creatures/StateMachine/AttackState.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class AttackState : IState
+{
+    private AngryBall parent;
+    private float attackCooldown = 1;
+    private float extraRange = .1f;
+    public void Enter(AngryBall parent)
+    {
+        this.parent = parent;
+    }
+
+    public void Exit()
+    {
+        
+    }
+
+    public void Update()
+    {
+        if (parent.AttackTime >= attackCooldown && !parent.IsAttacking)
+        {
+            parent.AttackTime = 0;
+            parent.StartCoroutine(Attack());
+        }
+        if (parent.Target != null)
+        {
+            float distance = Vector2.Distance(parent.transform.position, parent.Target.position);
+            if(distance>=parent.AttackRange+extraRange&& !parent.IsAttacking)
+            {
+                parent.ChangeState(new FollowState());
+            }
+        }
+        else
+        {
+            parent.ChangeState(new StaticState());
+        }
+    }
+
+    public IEnumerator Attack()
+    {
+        parent.CanDoDamage = true;
+        parent.IsAttacking = true;
+        parent.Animator.SetInteger("x", 2);
+        yield return new WaitForSeconds(parent.Animator.GetCurrentAnimatorStateInfo(2).length);
+        parent.IsAttacking = false;
+    }
+}
